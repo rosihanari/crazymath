aead84d6b3aa453edc840ff3e6fbe1f4a26065df
Rosihan Ari Yuana <rosihanari@gmail.com>
2019-04-12 06:24:07 +0700

    Update scores.sql

--- scores.sql
+++ scores.sql
@@ -3,8 +3,5 @@ CREATE TABLE `scores` (
   `username` varchar(30) DEFAULT NULL,
   `score` int(11) DEFAULT NULL,
   `playtime` datetime DEFAULT NULL,
-  `ipaddress` varchar(15) DEFAULT NULL,
-  `country` varchar(100) DEFAULT NULL,
-  `isp` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
 )
